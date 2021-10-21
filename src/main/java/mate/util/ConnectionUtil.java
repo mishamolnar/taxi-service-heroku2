f7963cc0824e826c7b7e90d6a71a8299aabc3eed
mishamolnar <michaellmuller423@gmail.com>
2021-10-21 17:53:18 +0300

    db configured

--- src/main/java/mate/util/ConnectionUtil.java
+++ src/main/java/mate/util/ConnectionUtil.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class ConnectionUtil {
-    private static final String URL = "jdbc:mysql://localhost:3306/taxi";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "1234";
+    private static final String URL = "jdbc:mysql://remotemysql.com:3306/iC9O9lZIJh";
+    private static final String USERNAME = "iC9O9lZIJh";
+    private static final String PASSWORD = "bkC8KUqlA0";
     private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
 
     static {
